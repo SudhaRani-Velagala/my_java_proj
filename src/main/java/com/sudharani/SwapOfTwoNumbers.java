6079efe849c34c1507c4bbdad4e0fec592d85a8a
sudharani-velagala <sudha7912@gmailcom>
2022-09-16 22:25:56 +0530

    Added Swap Of 2 numbers

---
+++ src/main/java/com/sudharani/SwapOfTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.sudharani;
+
+public class SwapOfTwoNumbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("before Swap a="+a+ "and b="+b );
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swap a="+a+"and b="+b);
+    }
+}
